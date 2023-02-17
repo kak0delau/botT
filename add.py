25338c93b87ee918e3a2ad3c694c75d23d2bbf4e
kak0delau <logisika112131@gmail.com>
2023-02-17 20:07:32 +0300

    YOI

---
+++ add.py
@@ -0,0 +1,18 @@
+import telebot
+import time
+from config import TOKEN
+
+bot = telebot.TelBot(TOKEN)
+
+
+
+@bot.message_handler(content_types=["text"])
+def text(message):
+    if message.text.lower() == "привет":
+      bot.sand_message(message.chat.id , "Привет от бота!")
+      bot.sand_message(message.chat.id, "Как твои дела? (хорошо/плохо)")
+    if message.text.lower() == "хорошо":
+      bot.sand_message(message.chat.id , "Супер!")
+    if message.text.lower() == "плохо":
+      bot.sand_message(message.chat.id, "Печально :(")
+bot.polling()
